2d65ae1ca2043f2e1c2d7eb312c4289be20d4504
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-12-02 15:54:02 +0100

    add event for when file input changes

--- site/index.js
+++ site/index.js
@@ -1,10 +1,18 @@
 import("./node_modules/photon-wasm/photon_wasm.js").then((photon) => {
 
+    let imageInput = document.getElementById('image-input')
+    let sourceImage = document.getElementById('src-image')
+
+    imageInput.onchange = e => {
+        const [image] = imageInput.files
+        if (image) sourceImage.src = URL.createObjectURL(image)
+    }
+
     function filterImage() {
         // Create a canvas and get a 2D context from the canvas
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
-        var myImage = document.getElementById("myImage");
+        let canvas = document.getElementById("canvas");
+        let ctx = canvas.getContext("2d");
+        let myImage = document.getElementById("myImage");
 
 
         scaleToFit(myImage, ctx);
@@ -19,14 +27,14 @@ import("./node_modules/photon-wasm/photon_wasm.js").then((photon) => {
         photon.putImageData(canvas, ctx, rust_image)
     }
 
-    filterImage()
+    // filterImage()
 
-    function scaleToFit(img, ctx){
+    function scaleToFit(img, ctx) {
         // get the scale
-        var scale = Math.min(canvas.width / img.width, canvas.height / img.height);
+        let scale = Math.min(canvas.width / img.width, canvas.height / img.height);
         // get the top left position of the image
-        var x = (canvas.width / 2) - (img.width / 2) * scale;
-        var y = (canvas.height / 2) - (img.height / 2) * scale;
+        let x = (canvas.width / 2) - (img.width / 2) * scale;
+        let y = (canvas.height / 2) - (img.height / 2) * scale;
         ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
     }
 });
