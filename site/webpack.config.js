d201c4843d6f68edcc013d6f6523c3c0c14a93b6
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-12-13 17:19:07 +0100

    add tailwind rules to config

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,17 +1,41 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: "./index.js",
+    entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "index.js",
+        filename: "bundle.js",
+        clean: true
     },
     devServer: {
         static: {
-            directory: path.resolve(__dirname, "./")
-        }
+            directory: path.resolve(__dirname, "dist"),
+        },
+        port: 8000
     },
     mode: "development",
     experiments: {
         asyncWebAssembly: true
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+    module: {
+        rules: [
+            {
+                test: /\.js$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ],
+    },
 };
