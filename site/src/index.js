74087d86fa963c71349cf48532141e74399a515d
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-12-18 17:52:00 +0100

    add jimp

--- site/src/index.js
+++ site/src/index.js
@@ -1,9 +1,59 @@
 import './styles.css'
-// import Caman from 'caman'
-// console.log(Caman)
 import Jimp from 'jimp'
 
-console.log(Jimp)
+
+const finalImage = document.getElementById("final-image")
+const finalImageCtx = finalImage.getContext("2d")
+const srcImage = document.getElementById("src-image")
+const srcImageCtx = srcImage.getContext("2d")
+const imageInput = document.getElementById('image-input')
+const jimpImage = document.getElementById("jimp-image")
+
+
+imageInput.onchange = e => {
+    const reader = new FileReader()
+    reader.onload = e => {
+        const image = new Image()
+
+        image.onload = () => {
+            // paint white over the canvas to cover previous image
+            finalImageCtx.fillStyle = "white"
+            finalImageCtx.fillRect(0, 0, finalImage.width, finalImage.height)
+
+            const jimage = Jimp.read(image.src)
+
+            jimage.then((result) => {
+                result.blur(20)
+
+                console.log(result)
+
+                result.getBase64Async(Jimp.MIME_JPEG).then((base64string) => {
+                    console.log(base64string)
+                    jimpImage.src = base64string
+                    finalImageCtx.drawImage(jimpImage, 0, 0)
+                })
+            }
+            )
+
+            // paint the src image onto the canvas
+            scaleToFit(image, srcImageCtx, srcImage)
+        }
+        image.src = e.target.result
+    }
+    reader.readAsDataURL(e.target.files[0]);
+}
+
+
+function scaleToFit(img, ctx, canvas) {
+    // get the scale
+    let scale = Math.min(canvas.width / img.width, canvas.height / img.height)
+    // get the top left position of the image
+    let x = (canvas.width / 2) - (img.width / 2) * scale
+    let y = (canvas.height / 2) - (img.height / 2) * scale
+    ctx.drawImage(img, x, y, img.width * scale, img.height * scale)
+}
+
+
 // import("../node_modules/photon-wasm/photon_wasm.js").then((photon) => {
 
 //     const imageInput = document.getElementById('image-input')
