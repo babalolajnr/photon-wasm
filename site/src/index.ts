a64d5dab2e292cf963160ed63ada2001c032301c
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-12-31 06:40:43 +0100

    move photon methons to photonfunctions class

--- site/src/index.ts
+++ site/src/index.ts
@@ -1,21 +1,20 @@
 import './styles.css'
-import Jimp from 'jimp'
 import { JimpFunctions } from './jimpFunctions'
-import * as photon from 'photon-wasm'
 import Toastr from 'toastr'
+import PhotonFunctions from './photonFunctions'
 
 const srcImage = <HTMLImageElement>document.getElementById("src-image")
+const srcImagePlaceholder = document.getElementById('src-image-placeholder')
+const imageInput = document.getElementById('image-input')
 
 const wasmResult = <HTMLCanvasElement>document.getElementById("wasm-result")
-const ctx = wasmResult.getContext('2d')
-const imageInput = document.getElementById('image-input')
-const jsResult = <HTMLImageElement>document.getElementById("js-result")
-const srcImagePlaceholder = document.getElementById('src-image-placeholder')
 const wasmResultPlaceholder = document.getElementById('wasm-result-placeholder')
-const jsResultPlaceholder = document.getElementById('js-result-placeholder')
-const jsResultTitle = document.getElementById('js-result-title')
 const wasmResultTitle = document.getElementById('wasm-result-title')
 const canvasImageDimensionReference = <HTMLImageElement>document.getElementById('canvas-image-dimension-reference')
+
+const jsResult = <HTMLImageElement>document.getElementById("js-result")
+const jsResultPlaceholder = document.getElementById('js-result-placeholder')
+const jsResultTitle = document.getElementById('js-result-title')
 
 let srcImageUrl: string;
 
@@ -36,7 +35,8 @@ const loadImage = function () {
 async function blur() {
 
     let t0wasm = performance.now()
-    photonBlur()
+    let photonFunctions = new PhotonFunctions(srcImage, srcImageUrl, wasmResult, wasmResultPlaceholder, wasmResultTitle, canvasImageDimensionReference)
+    photonFunctions.blur()
     let tfwasm = performance.now() - t0wasm
     console.log('wasm time: ' + tfwasm)
     Toastr.info(`Wasm run time: ${(tfwasm / 1000).toFixed(5)} s`, undefined, {
@@ -55,58 +55,5 @@ async function blur() {
 
 }
 
-
-
-/**
- * Blurs image and places it on the canvas
- */
-function photonBlur() {
-    resetCanvasImageDimensionReference()
-
-    canvasImageDimensionReference.src = srcImageUrl
-    canvasImageDimensionReference.onload = () => {
-
-        // Get image dimensions from the image element to ensure the canvas fits the image
-        wasmResult.height = canvasImageDimensionReference.height
-        canvasImageDimensionReference.classList.add('hidden')
-
-
-        // hide the placeholder and display image
-        wasmResultPlaceholder.classList.add('hidden')
-        wasmResult.classList.remove('hidden')
-        wasmResultTitle.classList.remove('hidden')
-        wasmResultTitle.classList.add('flex')
-
-        scaleToFill(srcImage, ctx, wasmResult)
-        const photonImage = photon.open_image(wasmResult, ctx)
-        photon.gaussian_blur(photonImage, 1)
-        photon.putImageData(wasmResult, ctx, photonImage)
-    }
-}
-
-/**
- * Reset the dimensions of the image reference element so that
- * the canvas does not get distorted after changing the reference
- * image
- */
-function resetCanvasImageDimensionReference() {
-    canvasImageDimensionReference.src = ''
-    canvasImageDimensionReference.classList.remove('hidden')
-}
-
 imageInput.onchange = loadImage
 
-/**
- * Scale image to fill canvas
- * @param  {HTMLImageElement} img
- * @param  {CanvasRenderingContext2D} ctx
- * @param  {HTMLCanvasElement} canvas
- */
-const scaleToFill = function (img: HTMLImageElement, ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
-    // get the scale
-    let scale = Math.max(canvas.width / img.width, canvas.height / img.height);
-    // get the top left position of the image
-    let x = (canvas.width / 2) - (img.width / 2) * scale;
-    let y = (canvas.height / 2) - (img.height / 2) * scale;
-    ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
-}
