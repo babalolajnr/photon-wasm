c2f96ea2e9173f58b98d5176146d14f025ca8961
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-01-01 08:00:58 +0100

    rename class

--- site/src/ToastrRunTimeDisplay.ts
+++ site/src/ToastrRunTimeDisplay.ts
@@ -1,8 +1,8 @@
-import RunTimeLogger from "./RunTimeLogger";
 import Toastr from 'toastr'
+import RunTimeDisplay from "./RunTimeDisplay";
 
 
-export default class ToastrRunTimeLogger implements RunTimeLogger {
+export default class ToastrRunTimeDisplay implements RunTimeDisplay {
 
     show(message: string): void {
         Toastr.info(message, undefined, {
