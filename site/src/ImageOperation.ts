0d567b5750c6b3d223689aa65cbb4dcd8f50edec
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-01-07 09:51:27 +0100

    add flipv

--- site/src/ImageOperation.ts
+++ site/src/ImageOperation.ts
@@ -2,4 +2,5 @@ export default interface ImageOperation {
     blur(): void | Promise<void>
     grayscale(): void | Promise<void>
     invert(): void | Promise<void>
+    flipv(): void | Promise<void>
 }
